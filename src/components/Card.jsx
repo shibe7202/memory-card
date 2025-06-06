4cd110273a1af9d851995efca312644981f9779a
shibe7202 <46942349+shibe7202@users.noreply.github.com>
2025-06-06 15:10:32 -0700

    Add effect to fetch data in Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+
+function Card({ imageID, onClick }) {
+    const [data, setData] = useState(null)
+    const url = 'https://api.artic.edu/api/v1/artworks/' + imageID + '?fields=id,title,image_id'
+    const imageUrl = data ? data.config.iiif_url + '/' + data.data.image_id + '/full/843,/0/default.jpg' : null
+    const title = data ? data.data.title : null
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(url)
+            const result = await response.json()
+            setData(result)
+        }
+        fetchData()
+    }, [url])
+
+    return (
+        <div className='card' onClick={onClick}>
+            <img src={imageUrl} alt={imageID} id='' />
+            <p id=''>{title}</p>
+        </div>
+    )
+}
+
+export default Card
